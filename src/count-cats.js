8494ec684299d06c2ac874a3d2e231b0baab5d6e
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2022-10-17 23:39:17 +0200

    fix: solutions

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(matrix) {
   let counter = 0;
   matrix.forEach(element => { element.forEach(item => {
     (item == '^^') ? counter++ : counter;
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
+  calculateDepth(arr) {
     let arraysCounter;
 
     let deepCounter = 1;
