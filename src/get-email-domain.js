2ba1f07c9675b22ba46ad00e46a42efe75be623e
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-04-16 16:32:18 +0200

    feat: add solution for get email domain task

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+
+  let emailContent = email.split('@');
+
+  let domain = emailContent[emailContent.length - 1];
+  return domain;
+
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,58 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  // if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+
+  // let newArr = [];
+  // let i = 0;
+
+  // while (i < arr.length) {
+  //   if ((arr[0] === '--double-prev' || '--discard-prev') || (arr[arr.length - 1] === '--double-next' || '--discard-next')) {
+  //       i++;
+  //       continue;
+  //   }
+
+  //   if (arr[i] === '--double-prev') {
+  //     newArr.push(arr[i-1]);
+  //     i++;
+  //     continue;
+  //   }
+
+  //   if (arr[i] === '--double-next') {
+  //     newArr.push(arr[i+1]);
+  //     i++;
+  //     continue;
+  //   }
+
+  //   if (arr[i] === '--discard-prev') {
+  //       newArr.pop();
+  //       i++;
+  //     continue;
+  //   }
+  //   if (arr[i] === '--discard-next') {
+  //       i = i+2;
+  //     continue;
+  //     // i++;
+  //   }
+
+  //   newArr.push(arr[i]);
+  //   i++;
+  // }
+
+  // return newArr;
+
 }
 
 module.exports = {
